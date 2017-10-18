8630725d0918803c5fd81d2499b39b9318121308
Rob Dowers <rdowers@gmail.com>
2017-10-18 00:58:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,3 +17,15 @@ function logWhisper(string) {
   console.log(string)
 
 }
+
+function sayHiToGrandma(string) {
+  if (string.toUpperCase() === string) {
+    //if uppercase is yes, then...
+    return "YES INDEED!"
+  } else if (string.toLowerCase() === string) {
+    //if lowercase is yes, then...
+    return "I can't hear you!"
+  } else if (string === "I love you, Grandma.") {
+    return "I love you too."
+  }
+}
